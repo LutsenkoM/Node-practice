e1dfcccb13a6a5b297bfd6fcb72268d98ec10b94
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2020-04-11 10:58:48 +0300

    Securely storing pass, Logging in Users

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -13,3 +13,17 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port' + port)
 });
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const pass = 'Red12345!';
+    const hashedPass = await bcrypt.hash(pass, 8);
+    console.log(pass);
+    console.log(hashedPass);
+
+    const isMatch = await bcrypt.compare("Red12345!", hashedPass);
+    console.log(isMatch);
+};
+
+myFunction();
